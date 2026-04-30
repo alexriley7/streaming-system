d76e5fcced17697c5ae26e3b61bf92331ef79ad2
Ale <alejandrorobledo.00@gmail.com>
2026-04-30 07:33:35 -0300

    added feature flag

--- producer/src/main/java/com/example/TransactionProducer.java
+++ producer/src/main/java/com/example/TransactionProducer.java
@@ -45,15 +45,15 @@ public class TransactionProducer {
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
         // ✅ FEATURE FLAG
-        boolean enabled = Boolean.parseBoolean(
-                System.getenv().getOrDefault("ENABLE_PRODUCER", "true")
-        );
+        //boolean enabled = Boolean.parseBoolean(
+        //        System.getenv().getOrDefault("ENABLE_PRODUCER", "true")
+        //);
 
         System.out.println("Starting transaction producer...");
         System.out.println("ENABLE_PRODUCER = " + enabled);
 
         try {
-
+        /*
             if (!enabled) {
                 // 🧠 Keep pod alive but do nothing
                 System.out.println("Producer DISABLED. Idling...");
@@ -61,7 +61,8 @@ public class TransactionProducer {
                     Thread.sleep(60000);
                 }
             }
-            
+            */
+
             while (true) {
 
                 Transaction tx = generateTransaction();
