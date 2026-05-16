55b1749f55525b80b2d1cc80e5fe567a36298879
Ale <alejandrorobledo.00@gmail.com>
2026-05-16 05:22:32 -0300

    added Shadow Flink logic

--- flink-transformation/src/main/java/com/example/FlinkTransformationJob.java
+++ flink-transformation/src/main/java/com/example/FlinkTransformationJob.java
@@ -107,7 +107,7 @@ public class FlinkTransformationJob {
 
         consumerProps.setProperty(
                 "auto.offset.reset",
-                "earliest"
+                "latest"
         );
 
         // ====================================================
--- shadow-flink-transformation/src/main/java/com/example/ShadowFlinkTransformationJob.java
+++ shadow-flink-transformation/src/main/java/com/example/ShadowFlinkTransformationJob.java
@@ -107,7 +107,7 @@ public class ShadowFlinkTransformationJob {
 
         consumerProps.setProperty(
                 "auto.offset.reset",
-                "earliest"
+                "latest"
         );
 
         // ====================================================
