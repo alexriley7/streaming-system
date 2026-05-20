71825a977775aeaf3d77334e6b97d6adacc76c58
Ale <alejandrorobledo.00@gmail.com>
2026-05-20 09:35:30 -0300

    added Shadow Flink logic

--- producer/src/main/java/com/example/Transaction.java
+++ producer/src/main/java/com/example/Transaction.java
@@ -1,13 +1,22 @@
 package com.example;
 
 class Transaction {
+
+    public String eventId;
+
     public String userId;
     public String transactionId;
     public double amount;
     public String currency;
     public long timestamp;
 
-    public Transaction(String userId, String transactionId, double amount, String currency, long timestamp) {
+    public Transaction(
+            String userId,
+            String transactionId,
+            double amount,
+            String currency,
+            long timestamp
+    ) {
         this.userId = userId;
         this.transactionId = transactionId;
         this.amount = amount;
