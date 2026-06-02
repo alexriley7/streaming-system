d8be7a39cd29201065830a2090f9349fdffc94cd
Ale <alejandrorobledo.00@gmail.com>
2026-06-02 19:45:37 -0300

    added Shadow Flink logic

--- shadow-flink-transformation/src/main/java/com/example/ShadowFlinkTransformationJob.java
+++ shadow-flink-transformation/src/main/java/com/example/ShadowFlinkTransformationJob.java
@@ -64,7 +64,7 @@ class EnrichedTransaction extends Transaction {
 
 
 // ============================================================
-// MAIN JOB
+// MAIN JOB #LastUpdated
 // ============================================================
 
 public class ShadowFlinkTransformationJob {
